4069b1be98f10007bf83a1e055ff494f786f7a0d
Mahesh <reddy.mahesh001@gmail.com>
2017-04-27 01:25:24 -0500

    Table forms now with JSON

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule }   from '@angular/router';
+
 import { AppComponent } from './app.component';
+
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { ComplaintsListComponent } from './complaints-list/complaints-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComplaintsListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    AlertModule.forRoot(),
+    HttpModule,
+    RouterModule.forRoot([
+      {
+      path:'complaints',
+      component:ComplaintsListComponent
+     }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
