5451cca1f760eea5cd9ca7aa4b38c5ec4c2809d4
Mahesh <reddy.mahesh001@gmail.com>
2017-04-28 02:44:23 -0500

    Angular In memory Web Api

--- src/app/complaint.service.ts
+++ src/app/complaint.service.ts
@@ -1,31 +1,93 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from "@angular/http";
-import { Observable } from "rxjs/Observable";
-import 'rxjs/add/operator/catch';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/map';
-import { IComplaint } from "app/complaint";
+import 'rxjs/add/observable/of';
+
+import { IComplaint } from './complaint';
 
 @Injectable()
 export class ComplaintService {
-// private _complaintUrl='https://raw.githubusercontent.com/iMahesh/demo-form/master/src/api/complaints.json';
- private _complaintUrl='assets/complaints.json';
+    private baseUrl = 'api/complaints';
 
-constructor(private _http:Http){}
+    constructor(private http: Http) { }
 
-    getComplaints(): Observable<IComplaint[]>{
-        return this._http.get(this._complaintUrl)
-        .map((response: Response)=> <IComplaint[]> response.json())
-        .do(data=>console.log('All: '+JSON.stringify(data)))
-        .catch(this.handleError);
+    getComplaints(): Observable<IComplaint[]> {
+        return this.http.get(this.baseUrl)
+            .map(this.extractData)
+            .do(data => console.log('getComplaints: ' + JSON.stringify(data)))
+            .catch(this.handleError);
     }
+
     getComplaintById(id: number): Observable<IComplaint> {
-        return this.getComplaints()
-            .map((complaints: IComplaint[]) => complaints.find(p => p.id === id));
+        if (id === 0) {
+        return Observable.of(this.initializeComplaint());
+        // return Observable.create((observer: any) => {
+        //     observer.next(this.initializecomplaint());
+        //     observer.complete();
+        // });
+        };
+        const url = `${this.baseUrl}/${id}`;
+        return this.http.get(url)
+            .map(this.extractData)
+            .do(data => console.log('getComplaint: ' + JSON.stringify(data)))
+            .catch(this.handleError);
     }
 
-    private handleError(error: Response){
-      console.error(error);
-      return Observable.throw(error.json().error || 'Server Error');
+
+    saveComplaint(complaint: IComplaint): Observable<IComplaint> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        if (complaint.id === 0) {
+            return this.createComplaint(complaint, options);
+        }
+        return this.updateComplaint(complaint, options);
+    }
+
+    private createComplaint(complaint: IComplaint, options: RequestOptions): Observable<IComplaint> {
+        complaint.id = undefined;
+        return this.http.post(this.baseUrl, complaint, options)
+            .map(this.extractData)
+            .do(data => console.log('createComplaint: ' + JSON.stringify(data)))
+            .catch(this.handleError);
+    }
+
+    private updateComplaint(complaint: IComplaint, options: RequestOptions): Observable<IComplaint> {
+        const url = `${this.baseUrl}/${complaint.id}`;
+        return this.http.put(url, complaint, options)
+            .map(() => complaint)
+            .do(data => console.log('updateComplaint: ' + JSON.stringify(data)))
+            .catch(this.handleError);
+    }
+    private extractData(response: Response) {
+        let body = response.json();
+        return body.data || {};
+    }
+
+    private handleError(error: Response): Observable<any> {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
+
+    initializeComplaint(): IComplaint {
+        // Return an initialized object
+        return {
+          "id": 0,
+            "region": null,
+   "territory": null,
+   "grower": null,
+   "retailer": null,
+   "dateFiled": "2014-06-22",
+   "status": "Open",
+   "description":null
+   
+        };
     }
 }
